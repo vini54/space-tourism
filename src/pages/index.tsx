a6486b78953f8e6f8a8377f30a522a1ea45ac79f
vini54 <vini544@gmail.com>
2023-05-08 11:10:08 -0300

    fixing uppercase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import homeStyle from "@/styles/homeStyles.module.css";
 import { NextPageWithLayout } from "./_app";
-import { MenuLayout } from "@/Components/MenuLayout";
+import { MenuLayout } from "@/components/MenuLayout";
 
 const Home: NextPageWithLayout = () => {
   return (
