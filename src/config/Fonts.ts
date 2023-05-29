20fd4e786bd3db5ba37964c5bae2dd3a07706909
vini54 <vini544@gmail.com>
2023-05-29 13:25:12 -0300

    destination page

--- src/config/Fonts.ts
+++ src/config/Fonts.ts
@@ -1,6 +1,7 @@
 import { Bellefair } from "next/font/google";
 import { Barlow } from "next/font/google";
 import { Barlow_Condensed } from "next/font/google";
+import localFont from "next/font/local";
 
 const bellefair = Bellefair({
   weight: "400",
@@ -8,15 +9,51 @@ const bellefair = Bellefair({
   adjustFontFallback: false,
 });
 const barlow = Barlow({
-  weight: ["400", "500"],
+  weight: ["300", "400", "500"],
   subsets: ["latin"],
   adjustFontFallback: false,
 });
-const barlowCondensed = Barlow_Condensed({
-  weight: ["300", "400", "500", "600"],
-  subsets: ["latin"],
-  adjustFontFallback: false,
+
+const barlowCondensed = localFont({
+  src: [
+    {
+      path: "../../public/assets/Fonts/Barlow_Condensed/BarlowCondensed-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/Fonts/Barlow_Condensed//BarlowCondensed-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/Fonts/Barlow_Condensed/BarlowCondensed-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/Fonts/Barlow_Condensed/BarlowCondensed-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/Fonts/Barlow_Condensed/BarlowCondensed-ExtraBold.ttf",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/Fonts/Barlow_Condensed/BarlowCondensed-Black.ttf",
+      weight: "900",
+      style: "normal",
+    },
+  ],
 });
+
+// const barlowCondensed = Barlow_Condensed({
+//   weight: ["400", "500", "600", "700", "800"],
+//   subsets: ["latin"],
+//   adjustFontFallback: false,
+// });
 
 export const FontFamilies = {
   bellefair: bellefair,
---
+++ src/pages/destination/index.tsx
@@ -0,0 +1,197 @@
+import { MenuLayout } from "@/Components/MenuLayout";
+import { NextPageWithLayout } from "../_app";
+import styles from "./styles.module.css";
+import { useState } from "react";
+import Image from "next/image";
+
+type planetTargetOpt = "moon" | "mars" | "europa" | "titan";
+
+const Destination: NextPageWithLayout = () => {
+  const [planetTarget, setPlanetTarget] = useState<planetTargetOpt>("moon");
+  return (
+    <main
+      className={`${styles.main} min-[900px]:w-screen min-[900px]:h-screen flex items-end`}
+    >
+      <div
+        className={`${styles.container} flex flex-col justify-center gap-10 px-4 lg:px-[6%] max-[900px]:pt-32 max-[900px]:pb-8`}
+      >
+        <h3 className="text-xl md:text-3xl text-center md:text-left font-light uppercase">
+          <span className="ml-6 text-gray-700 font-extrabold">01</span> Pick
+          your destination
+        </h3>
+
+        <div
+          className={`${styles.wrap} px-0 lg:px-6 min-[900px]:flex-row flex-col flex max-[900px]:gap-12 gap-8 justify-evenly items-center`}
+        >
+          <div className="flex justify-center sm:w-auto w-3/4">
+            {planetTarget === "moon" && (
+              <PlanetImage src="image-moon.png" alt="moon" />
+            )}
+            {planetTarget === "mars" && (
+              <PlanetImage src="image-mars.png" alt="mars" />
+            )}
+            {planetTarget === "europa" && (
+              <PlanetImage src="image-europa.png" alt="europa" />
+            )}
+            {planetTarget === "titan" && (
+              <PlanetImage src="image-titan.png" alt="titan" />
+            )}
+          </div>
+
+          <div
+            className={`${styles.content} w-full flex flex-col max-[900px]:items-center gap-8 sm:w-2/5 sm:min-w-[445px]`}
+          >
+            <nav
+              className={`${styles.contentNav} max-[900px]:w-fit pb-2 flex justify-start items-center gap-6 text-sm sm:text-base  relative text-slate-300`}
+            >
+              <p
+                onClick={() => setPlanetTarget("moon")}
+                className={`${
+                  planetTarget === "moon" && "text-slate-50"
+                } cursor-pointer transition-colors duration-500`}
+              >
+                MOON
+              </p>
+              <p
+                onClick={() => setPlanetTarget("mars")}
+                className={`${
+                  planetTarget === "mars" && "text-slate-50"
+                } cursor-pointer transition-colors duration-500`}
+              >
+                MARS
+              </p>
+              <p
+                onClick={() => setPlanetTarget("europa")}
+                className={`${
+                  planetTarget === "europa" && "text-slate-50"
+                } cursor-pointer transition-colors duration-500`}
+              >
+                EUROPA
+              </p>
+              <p
+                onClick={() => setPlanetTarget("titan")}
+                className={`${
+                  planetTarget === "titan" && "text-slate-50"
+                } cursor-pointer transition-colors duration-500`}
+              >
+                TITAN
+              </p>
+
+              <span
+                className={`${
+                  planetTarget === "moon"
+                    ? "translate-x-0"
+                    : planetTarget === "mars"
+                    ? "sm:translate-x-20 translate-x-[70px]"
+                    : planetTarget === "europa"
+                    ? "sm:translate-x-40 translate-x-[138px]"
+                    : planetTarget === "titan"
+                    ? "sm:translate-x-[262px] translate-x-[230px]"
+                    : "translate-x-0"
+                } ${
+                  planetTarget === "europa" ? "w-[84px]" : "sm:w-16 w-[58px]"
+                } h-[2px] bg-slate-50 absolute bottom-0 -left-1 transition-all duration-500 ease-in`}
+              />
+            </nav>
+
+            {planetTarget === "moon" && (
+              <PlanetInfos
+                title="MOON"
+                desc="See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites."
+                distance="384,400 km"
+                time="3 days"
+              />
+            )}
+
+            {planetTarget === "mars" && (
+              <PlanetInfos
+                title="MARS"
+                desc="Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!"
+                distance="225 MIL. km"
+                time="9 months"
+              />
+            )}
+
+            {planetTarget === "europa" && (
+              <PlanetInfos
+                title="EUROPA"
+                desc="The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin."
+                distance="628 MIL. km"
+                time="3 years"
+              />
+            )}
+
+            {planetTarget === "titan" && (
+              <PlanetInfos
+                title="TITAN"
+                desc="The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn."
+                distance="1.6 BIL. km"
+                time="7 years"
+              />
+            )}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+type PlanetImageProps = {
+  src: string;
+  alt: string;
+};
+
+const PlanetImage = ({ src, alt }: PlanetImageProps) => {
+  return (
+    <Image
+      className={`${styles.pulse} aspect-square`}
+      src={"/assets/destination/" + src}
+      width={350}
+      height={350}
+      alt={alt}
+    />
+  );
+};
+
+type planetInfosProps = {
+  title: string;
+  desc: string;
+  distance: string;
+  time: string;
+};
+
+const PlanetInfos = ({ title, desc, distance, time }: planetInfosProps) => {
+  return (
+    <div
+      className={`${styles.contentInfos} ${styles.pulse} w-full flex flex-col max-[900px]:items-center max-[900px]:text-center gap-6`}
+    >
+      <h1 className="text-6xl md:text-8xl -ml-2">{title}</h1>
+
+      <p className="text-slate-400 text-base">{desc}</p>
+
+      <hr className="opacity-25 w-full" />
+
+      <div className="w-full flex sm:flex-row flex-col justify-start max-[900px]:justify-evenly gap-8">
+        <div className="min-[900px]:mr-[35%]">
+          <small className="text-sm text-slate-300 font-normal uppercase whitespace-nowrap">
+            AVG. DISTANCE
+          </small>
+          <h4 className="text-2xl uppercase whitespace-nowrap">{distance}</h4>
+        </div>
+
+        <div>
+          <small className="text-sm text-slate-300 font-normal uppercase whitespace-nowrap">
+            Est. travel time
+          </small>
+          <h4 className="text-2xl uppercase whitespace-nowrap">{time}</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Destination.getLayout = function getLayout(page) {
+  return <MenuLayout>{page}</MenuLayout>;
+};
+
+export default Destination;
